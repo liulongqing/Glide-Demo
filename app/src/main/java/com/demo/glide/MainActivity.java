eef4fa6ec9d0a22e595143baaecbe2e5f69f10a0
liulongqing <liu8881751@sina.com>
2018-10-31 16:47:13 +0800

    添加：picasso的用法

--- app/src/main/java/com/demo/glide/MainActivity.java
+++ app/src/main/java/com/demo/glide/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.demo.glide.Picasso.PicassoTransformation;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +29,21 @@ public class MainActivity extends AppCompatActivity {
         //picasso图片加载
         Picasso.with(this)
                 .load(imgUrl)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .resize(400,300)
+                .onlyScaleDown()//调用resize之后，只有当原始图片的尺寸大于resize设置的尺寸，resize才起作用
+                .fit()//fit 只对ImageView 有效,测量view的宽高填满view
+                .centerCrop()//居中裁剪
+                .rotate(60)//以0，0为原点旋转角度
+                //你要做图片高斯模糊、添加圆角、做度灰处理、圆形图片等等都可以通过Transformation来完成
+                .transform(new PicassoTransformation(this))
+                //设置tag标签,场景1 滑动列表进行图片加载时候做滑动监听处理，滑动不加载图片，停止滑动时在加载图片
+                //           场景2 图片流那种图片比较多的界面，当网络不好的时候 要退出是还没加载完，调用cancelTag停止加载
+                .tag("PhotoTag")
+                .memoryPolicy(MemoryPolicy.NO_CACHE,MemoryPolicy.NO_STORE) //静止内存缓存
+                .networkPolicy(NetworkPolicy.NO_CACHE)//跳过磁盘缓存
+
                 .into(imageView);
     }
 }
