2658355e8ba1e06eac9d631bf8508d999a40f212
liulongqing <liu8881751@sina.com>
2018-10-31 11:46:45 +0800

    添加：GlideApp类进行图片的加载处理

--- app/src/main/java/com/demo/glide/MainActivity.java
+++ app/src/main/java/com/demo/glide/MainActivity.java
@@ -1,13 +1,24 @@
 package com.demo.glide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String imgUrl = "http://pics.sc.chinaz.com/files/pic/pic9/201809/zzpic14004.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.act_main_iv);
+        //图片加载
+//        Glide.with(this).load(imgUrl).into(imageView);
+        GlideApp.with(this)
+                .load(imgUrl)
+                .placeholder(R.drawable.ic_launcher_background)//图片加载出来前，显示的图片
+                .error(R.drawable.ic_launcher_background)//图片加载失败后，显示的图片
+                .fitCenter()
+                .into(imageView);
     }
 }
---
+++ app/src/main/java/com/demo/glide/MyGlideAppModule.java
@@ -0,0 +1,8 @@
+package com.demo.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+
+@GlideModule
+public class MyGlideAppModule extends AppGlideModule {}
